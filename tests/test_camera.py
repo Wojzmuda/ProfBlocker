781a3a082778124297d9998ec22e6cc6469c1227
Wojtek <w.20.zmuda@gmail.com>
2026-04-29 23:56:20 +0200

    Camera config and tests added

---
+++ tests/test_camera.py
@@ -0,0 +1,34 @@
+import pytest
+import numpy as np
+from camera import Camera
+
+class TestCamera:
+    @pytest.fixture
+    def camera_obj(self):
+        cam = Camera(0)
+        yield cam
+        cam.release()
+
+    def test_camera_init(self):
+        camera = Camera(5)
+
+        assert camera._camera_index == 5
+        assert camera._video_capture is None
+
+    def test_camera_setup(self,camera_obj):
+        camera_obj.setup()
+
+        assert camera_obj._video_capture is not None
+        assert camera_obj._video_capture.isOpened() is True
+
+    def test_camera_get_frames(self, camera_obj):
+        camera_obj.setup()
+        frame = camera_obj.get_frame()
+
+        assert frame is not None
+        assert isinstance(frame, np.ndarray)
+        assert len(frame.shape) == 3
+        
+
+
+        

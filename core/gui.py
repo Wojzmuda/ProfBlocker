17de12d25b0f1a14664b514ce09832b781f10b69
Wojtek <w.20.zmuda@gmail.com>
2026-05-01 14:18:14 +0200

    Starting the GUI

---
+++ core/gui.py
@@ -0,0 +1,16 @@
+import customtkinter
+
+
+class GUI(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+        self.title("ProfBlocker")
+        self.geometry("800x500")
+
+
+
+
+class ConfigPanel(customtkinter.CTkFrame):
+    def __init__(self):
+        
+    
